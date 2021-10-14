9555b250034a33be261e89512aaa174b37472cbb
Iorweth Aen Seidhe <bukinich98@mail.ru>
2021-10-14 20:09:01 +0300

    Добавьте файлы проекта.

---
+++ WPF_EFCore/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_EFCore.Model;
+
+namespace WPF_EFCore
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<Client> Ckients { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=wpf_efcore;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ WPF_EFCore/Model/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_EFCore.Model
+{
+    class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int  DepAccId{ get; set; }
+        public int  DontDepAccId{ get; set; }
+
+
+    }
+}
